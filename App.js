83d91a68b03020cd9c99bfecac90aa61b96def7e
Daniel Speight <danisp1@mail.regent.edu>
2022-10-18 23:43:42 -0500

    Got the inspirational quote API to work!

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Component } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import axios from "axios";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const options = {
+  method: "GET",
+  url: "https://api.goprogram.ai/inspiration",
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "",
+      quoteAuthor: "",
+      test: "",
+    };
+
+    this.quoteTesting = this.quoteTesting.bind(this);
+  }
+
+  quoteTesting = async () => {
+    axios
+      .request(options)
+      .then(
+        function (response) {
+          this.setState({
+            quote: response.data.quote,
+            quoteAuthor: response.data.author,
+          });
+        }.bind(this)
+      )
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.quoteText}>{this.state.quote}</Text>
+        <Text style={styles.authorText}>{this.state.quoteAuthor}</Text>
+
+        <Button
+          onPress={this.quoteTesting}
+          title="Daily Inspirational Quote"
+          color="#841584"
+          accessibilityLabel="Get an inspirational quote."
+        />
+
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "darkslateblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  quoteText: {
+    textAlign: "center",
+    color: "floralwhite",
+    fontSize: 16,
+  },
+  authorText: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 14,
   },
 });
+
+export default App;
